a2d1e83756a9467cc2c6390686d50e39220a2869
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-04-07 13:42:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+
+const download = (url) =>{
+	return new Promise((resolve, reject)=>{
+		resolve(`<img src="${url}" />`);
+	})
+}
+const handleDownload = ()=>{
+	Promise.all([download(images[0].url), download(images[1].url), download(images[2].url)]).then((data)=>{
+		data.forEach((v)=>{
+			output.innerHTML += v;
+		})
+	})
+}
+
+btn.addEventListener('click', handleDownload);
